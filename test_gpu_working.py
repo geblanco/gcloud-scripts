79ddf31c3f8093720af84decb238deaa2ba31f17
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-02-18 13:35:07 +0100

    Fix gpu test for tf2

--- test_gpu_working.py
+++ test_gpu_working.py
@@ -1,8 +1,10 @@
-import tensorflow as tf
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
+import tensorflow as tf                                                                                                                                                                                                              
+tf.debugging.set_log_device_placement(True)                                                                                                                                                                                          
 
-with tf.Session() as sess:
-    print (sess.run(c))
+with tf.device('/GPU:0'): 
+  a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]) 
+  b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]) 
+  
+c = tf.matmul(a, b) 
+print(c) 
+               
