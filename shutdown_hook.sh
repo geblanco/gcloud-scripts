ed31b3e343a700046ddd384baedae733ce7a881a
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-05-18 03:41:20 +0200

    Add proxy server

--- shutdown_hook.sh
+++ shutdown_hook.sh
@@ -12,8 +12,13 @@ done
 
 if [ -f ~/.server_data ]; then
   . ~/.server_data
+  proxy_data=""
+  if [[ ! -z "${HOP_SERVER}" ]]; then
+    proxy_data="-e \"ssh -p 22 -A ${HOP_SERVER} ssh\""
+  fi
   # This is a good moment to backup important stuff... i.e.: train log, results...
-  rsync -avrzP "${TO_BACKUP_DIR}" "${BACKUP_SERVER}:/${REMOTE_BACKUP_DIR}"
+  # ToDo := Add files from filelist
+  rsync "${proxy_data}" -avrzP "${TO_BACKUP_DIR}" "${BACKUP_SERVER}:/${REMOTE_BACKUP_DIR}"
 fi
 
 sudo shutdown -h now
